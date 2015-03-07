d707eff4686f0c1d4059276e8007a72eafff2e71
Daniel <dan.m.bartel@gmail.com>
2015-03-06 21:19:20 -0600

    little bit of stylin

--- static/app/dashboard/dashboard.js
+++ static/app/dashboard/dashboard.js
@@ -3,6 +3,13 @@ define([
 	"angularRoute"], function(angular) {
 		return angular.module("agileApp.dashboard", ["ui.router"])
 			.controller("DashboardCtrl", ["$scope", function($scope) {
+
+				//filler data for display purposes
+				$scope.projects = [
+					"Project 1",
+					"Project 2",
+					"Project 3"
+				];
 				
 			}]);
 });
