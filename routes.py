c29c24e7d6e65790e228aee39f1fa832f3213ae2
edciri116 <edciri.116@gmail.com>
2015-03-25 22:06:30 -0500

    login and dashboard
    login works and crappy table is generated in the dashboard

--- routes.py
+++ routes.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, abort, g
+#import flask
 from config import app
 from controllers import *
 from github import Github
 import sqlite3
+import json
 
 # Connect db before request
-@app.before_request
-def before_request():
-	g.db = sqlite3.connect(app.config['DATABASE'])
+
+#@app.before_request
+#def before_request():
+#	g.db = sqlite3.connect(app.config['DATABASE'])
 
 
 #Close connection after request
@@ -30,9 +33,10 @@ def get_response(request, ctrl):
 
 
 #Home page 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def index():
     print 'running initial route'
+ 
     return render_template("landing.html")
 
 #these routes never run
@@ -48,7 +52,21 @@ def githubLogin():
 @app.route('/dashboard', methods = ['GET', 'POST'])
 def dashboardFunc():
     print 'running dashboard route'
-    pass
+    username = request.form['username']
+    password = request.form['password']
+    print username
+    print password
+    data = {}
+    try:
+        user = Github(str(username), str(password))
+    	for repo in user.get_user().get_repos():
+            print repo.name
+            print repo.description
+            data[repo.name] = repo.description
+
+    except:
+        return render_template("landing.html")
+    return render_template("dashboard.html", data = json.dumps(data))
 
 @app.route('/planning', methods = ['POST'])
 def planningFunc():
