42144ad08cb33435dc7b8547225be9a8dd230fd4
Daniel <dan.m.bartel@gmail.com>
2015-04-05 16:44:41 -0500

    js controllers pulls in issues for backlog

--- static/js/controllers/planningCtrl.js
+++ static/js/controllers/planningCtrl.js
@@ -1,3 +1,15 @@
-app.controller("planningCtrl", ["$scope", function($scope) {
-	$scope.test = "hi there";
+app.controller("planningCtrl", ["$scope", "$http", "$location", function($scope, $http, $location) {
+	$scope.backlog = [];
+    $scope.currentSprint = [];
+
+    var projectName = $location.path().split("/")[2] || "";
+
+    // Get the backlog
+    $http({
+        method: "GET",
+        url: "/issues/" + projectName
+    }).success(function(data,status,headers,config) {
+        $scope.backlog = data;
+    });
+
 }]);
