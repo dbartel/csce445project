ac79e37853b588efc023ecd0de3d206223a04a0a
Daniel <dan.m.bartel@gmail.com>
2015-04-05 16:43:50 -0500

    updated angular to use html5mode to parse links

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,13 @@
 // Bootstrap angular application
 
-var app = angular.module("agilApp", []);
+var app = angular.module("agilApp", [], function($locationProvider) {
+    //Turn on html5mode so we can access full url
+    $locationProvider.html5Mode(true);
+});
 
+//set custom angular brackets so we don't interfere with flask templating
 app.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[');
   $interpolateProvider.endSymbol(']}');
 }]);
+
