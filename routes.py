8faa31b97c7e5e4f50cea5cfd649b63baa61e098
Daniel <dan.m.bartel@gmail.com>
2015-04-05 21:21:24 -0500

    implemented backend routes for creating new sprints

--- routes.py
+++ routes.py
@@ -3,7 +3,6 @@ from flask.ext.github import GitHub
 
 #import flask
 from config import app
-from controllers import *
 import json
 
 # Initialize flask-github
@@ -38,7 +37,7 @@ def before_request():
 @app.route("/login")
 def login():
     if session.get("user_id", None) is None:
-        return github.authorize()
+        return github.authorize("repo")
     else:
         return redirect(url_for("dashboardFunc"))
 
@@ -115,15 +114,69 @@ def sprintFunc():
 ## "API" Routes
 ## We shouldn't need a backend db, but we should build up some backend routes to hit the GitHub API so our pages can asynchronously load data
 
-# Get the list of issues for a project
+# Get the list of all issues for a project
+#Pass in a sprintid to get the issues for that sprint
 @app.route("/issues/<project>", methods=["GET"])
 def issues(project):
-    if project is not None:
-        api_endpoint = "repos/{0}/{1}/issues".format(g.username,project)
+        sprintId = request.args.get("sprintid", "*")
+        api_endpoint = "repos/{0}/{1}/issues?milestone={2}".format(g.username, project, sprintId)
         issues = github.get(api_endpoint)
         return json.dumps(issues)
+
+
+# Get the backlog for a project (open issues not assigned to any sprint)
+@app.route("/backlog/<project>", methods=["GET", "POST"])
+def backlogFn(project):
+    checkAuth()
+    api_endpoint = "repos/{0}/{1}/issues?milestone=none".format(g.username, project)
+    backlog = github.get(api_endpoint)
+    return json.dumps(backlog)
+
+@app.route("/currentsprint/<project>", methods=["GET", "POST"])
+def currentSprintFn(project):
+    checkAuth()
+    if request.method == "POST":
+        sprint_id = request.args.get("sprintid", None)
+        issue_id = request.args.get("issueid", None)
+        if sprint_id is None or issue_id is None:
+            abort(400)
+        else:
+            params = {
+                "milestone":sprint_id
+            }
+
+            jparam = json.dumps(params)
+            api_endpoint = "repos/{0}/{1}/issues/{2}".format(g.username,project,issue_id)
+            github.patch(api_endpoint, data=jparam)
+            return "Success", 201
     else:
-        # if no project specified, throw a 400 Bad Request
         abort(400)
 
+#Get the list of open sprints
+#POST with the params "title" and "due_on"
+@app.route("/sprints/<project>", methods=["GET", "POST"])
+def sprintFn(project):
+    checkAuth()
+    if request.method == "POST":
+        title = request.args.get("title", None)
+        due_on = request.args.get("due_on", None)
 
+        # If the params aren't there, send a bad request
+        if title is None or due_on is None:
+            abort(400)
+        else:
+            # params = "title={0}".format(title,due_on)
+
+            api_endpoint = "repos/{0}/{1}/milestones".format(g.username, project)
+
+            params ={
+                "title": title,
+                "due_on": due_on
+            }
+            github.post(api_endpoint, params)
+            # github.raw_request("POST", api_endpoint, params)
+            return "Success", 201
+    else:
+        api_endpoint = "repos/{0}/{1}/milestones".format(g.username, project)
+        sprints = github.get(api_endpoint)
+        return json.dumps(sprints)
